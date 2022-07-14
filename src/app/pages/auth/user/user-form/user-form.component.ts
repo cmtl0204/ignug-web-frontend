6a22864813a10f85056a1eaba83cc58976072423
cmtl0204 <cesar.tamayo0204@gmail.com>
2022-07-14 13:32:58 -0500

    fix bugs

--- src/app/pages/auth/user/user-form/user-form.component.ts
+++ src/app/pages/auth/user/user-form/user-form.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CreateUserDto, UpdateUserDto} from '@models/auth';
-import {CatalogueModel, ColumnModel} from '@models/core';
+import {CatalogueModel} from '@models/core';
 import {UsersHttpService} from '@services/auth';
 import {CataloguesHttpService, CoreService, MessageService} from '@services/core';
 import {OnExitInterface} from '@shared/interfaces';
