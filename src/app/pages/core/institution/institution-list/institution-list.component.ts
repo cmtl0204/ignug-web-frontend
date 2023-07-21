d280863e49dbee6a516e101a5d63d1a4c4397849
1755536099 <maf.chimborazo@yavirac.edu.ec>
2023-07-21 14:40:51 -0500

    cambios institution

--- src/app/pages/core/institution/institution-list/institution-list.component.ts
+++ src/app/pages/core/institution/institution-list/institution-list.component.ts
@@ -1,15 +1,14 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {Router} from '@angular/router';
 import {MenuItem, PrimeIcons} from "primeng/api";
 import {ColumnModel, InstitutionModel, SelectInstitutionDto, PaginatorModel} from '@models/core';
-import {BreadcrumbService, CoreService, InstitutionsHttpService, MessageService} from '@services/core';
+import {BreadcrumbService, CoreService, InstitutionsHttpService, MessageService,RoutesService} from '@services/core';
 
 @Component({
   selector: 'app-institution-list',
   templateUrl: './institution-list.component.html',
   styleUrls: ['./institution-list.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class InstitutionListComponent implements OnInit {
   protected readonly PrimeIcons = PrimeIcons;
@@ -18,19 +17,20 @@ export class InstitutionListComponent implements OnInit {
   protected isActionButtons: boolean = false;
   protected paginator: PaginatorModel;
   protected search: FormControl = new FormControl('');
-  protected selectedInstitution: SelectInstitutionDto = {};
-  protected selectedInstitutions: InstitutionModel[] = [];
-  protected institutions: InstitutionModel[] = [];
+  protected selectedItem: SelectInstitutionDto = {};
+  protected selectedItems: InstitutionModel[] = [];
+  protected items: InstitutionModel[] = [];
 
   constructor(
-    public coreService: CoreService,
     private breadcrumbService: BreadcrumbService,
+    public coreService: CoreService,
     public messageService: MessageService,
     private router: Router,
+    private routesService: RoutesService,
     private institutionsHttpService: InstitutionsHttpService,
   ) {
     this.breadcrumbService.setItems([
-      {label: 'Institucion'},
+      {label: 'Institución'},
     ]);
     this.paginator = this.coreService.paginator;
     this.search.valueChanges.subscribe(value => {
@@ -44,17 +44,18 @@ export class InstitutionListComponent implements OnInit {
     this.findAll();
   }
 
+  /** Load Data **/
   findAll(page: number = 0) {
     this.institutionsHttpService.findAll(page, this.search.value)
       .subscribe((response) => {
         this.paginator = response.pagination!;
-        this.institutions = response.data
+        this.items = response.data
       });
   }
-
+  
+  /** Build Data **/
   get buildColumns(): ColumnModel[] {
     return [
-      {field: 'address', header: 'Dirección'},
       {field: 'cellphone', header: 'Teléfono'},
       {field: 'code', header: 'Código'},
       {field: 'email', header: 'Email'},
@@ -70,51 +71,40 @@ export class InstitutionListComponent implements OnInit {
         label: 'Actualizar',
         icon: PrimeIcons.PENCIL,
         command: () => {
-          if (this.selectedInstitution?.id) this.redirectEditForm(this.selectedInstitution.id);
+          if (this.selectedItem?.id) this.redirectEditForm(this.selectedItem.id);
         },
       },
       {
         label: 'Eliminar',
         icon: PrimeIcons.TRASH,
         command: () => {
-          if (this.selectedInstitution?.id) this.remove(this.selectedInstitution.id);
+          if (this.selectedItem?.id) this.remove(this.selectedItem.id);
         },
       },
       {
         label: 'Ocultar',
         icon: PrimeIcons.LOCK,
         command: () => {
-          if (this.selectedInstitution?.id) this.hide(this.selectedInstitution.id);
+          if (this.selectedItem?.id) this.hide(this.selectedItem.id);
         },
       },
       {
-        label: 'Reactivar',
+        label: 'Mostrar',
         icon: PrimeIcons.LOCK_OPEN,
         command: () => {
-          if (this.selectedInstitution?.id) this.reactivate(this.selectedInstitution.id);
+          if (this.selectedItem?.id) this.reactivate(this.selectedItem.id);
         },
       }
     ];
   }
-
-  paginate(event: any) {
-    this.findAll(event.page);
-  }
-
-  redirectCreateForm() {
-    this.router.navigate(['/administration/institutions', 'new']);
-  }
-
-  redirectEditForm(id: string) {
-    this.router.navigate(['/administration/institutions', id]);
-  }
-
+  
+  /** Actions **/
   remove(id: string) {
     this.messageService.questionDelete()
       .then((result) => {
         if (result.isConfirmed) {
-          this.institutionsHttpService.remove(id).subscribe((institution) => {
-            this.institutions = this.institutions.filter(item => item.id !== institution.id);
+          this.institutionsHttpService.remove(id).subscribe(() => {
+            this.items = this.items.filter(item => item.id !== id);
             this.paginator.totalItems--;
           });
         }
@@ -124,33 +114,48 @@ export class InstitutionListComponent implements OnInit {
   removeAll() {
     this.messageService.questionDelete().then((result) => {
       if (result.isConfirmed) {
-        this.institutionsHttpService.removeAll(this.selectedInstitutions).subscribe((institutions) => {
-          this.selectedInstitutions.forEach(institutionDeleted => {
-            this.institutions = this.institutions.filter(institution => institution.id !== institutionDeleted.id);
+        this.institutionsHttpService.removeAll(this.selectedItems).subscribe(() => {
+          this.selectedItems.forEach(itemDeleted => {
+            this.items = this.items.filter(item => item.id !== itemDeleted.id);
             this.paginator.totalItems--;
           });
-          this.selectedInstitutions = [];
+          this.selectedItems = [];
         });
       }
     });
   }
 
-  selectInstitution(institution: InstitutionModel) {
-    this.isActionButtons = true;
-    this.selectedInstitution = institution;
-  }
-
   hide(id: string) {
-    this.institutionsHttpService.hide(id).subscribe(institution => {
-      const index = this.institutions.findIndex(institution => institution.id === id);
-      this.institutions[index].isVisible = false;
+    this.institutionsHttpService.hide(id).subscribe(item => {
+      const index = this.items.findIndex(item => item.id === id);
+      this.items[index].isVisible = false;
     });
   }
 
   reactivate(id: string) {
-    this.institutionsHttpService.reactivate(id).subscribe(institution => {
-      const index = this.institutions.findIndex(institution => institution.id === id);
-      this.institutions[index] = institution;
+    this.institutionsHttpService.reactivate(id).subscribe(item => {
+      const index = this.items.findIndex(item => item.id === id);
+      this.items[index].isVisible = true;
     });
   }
+
+  /** Select & Paginate **/
+  selectItem(item: InstitutionModel) {
+    this.isActionButtons = true;
+    this.selectedItem = item;
+  }
+  paginate(event: any) {
+    this.findAll(event.page);
+  }
+
+  /** Redirects **/
+  redirectCreateForm() {
+    this.router.navigate([this.routesService.institutions, 'new']);
+  }
+
+  redirectEditForm(id: string) {
+    this.router.navigate([this.routesService.institutions, id]);
+  }
 }
+
+
