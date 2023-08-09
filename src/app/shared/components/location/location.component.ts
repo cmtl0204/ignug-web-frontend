969fc0b824dfd689e7785c78c604a4cbfd7246f7
1724909443 <ctamayo@yavirac.edu.ec>
2023-08-09 09:59:44 -0500

    add location

--- src/app/shared/components/location/location.component.ts
+++ src/app/shared/components/location/location.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {CoreService, MessageService} from "@services/core";
 
 @Component({
   selector: 'app-location',
@@ -6,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./location.component.scss']
 })
 export class LocationComponent {
+  protected form: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, protected messageService: MessageService, protected coreService: CoreService,) {
+    this.form = this.newForm;
+  }
+
+  get newForm() {
+    return this.formBuilder.group({
+      id: [null],
+      country: [null],
+      province: [null],
+      canton: [null],
+      parrish: [null],
+    })
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      if (this.idField.value) {
+        // this.update(this.form.value);
+      } else {
+        // this.create(this.form.value);
+      }
+    } else {
+      this.form.markAllAsTouched();
+      this.messageService.errorsFields;
+    }
+  }
+
+  get idField() {
+    return this.form.controls['id'];
+  }
+
+  get countryField() {
+    return this.form.controls['country'];
+  }
+
+  get provinceField() {
+    return this.form.controls['province'];
+  }
+
+  get cantonField() {
+    return this.form.controls['canton'];
+  }
+
+  get parrishField() {
+    return this.form.controls['parrish'];
+  }
 }
