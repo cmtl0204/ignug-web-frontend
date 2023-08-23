9bd76fdbb3b5b3c1b402a4515e8bd0a4bcf12dd0
1755221270 <dam.carrera@yavira.edu.ec>
2023-08-23 10:13:11 -0500

    Create component form and list Student

--- src/app/pages/common/common-routing.module.ts
+++ src/app/pages/common/common-routing.module.ts
@@ -1,19 +1,28 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {NotFoundComponent} from './not-found/not-found.component';
-import {UnderMaintenanceComponent} from './under-maintenance/under-maintenance.component';
-import {AccessDeniedComponent} from './access-denied/access-denied.component';
+import {ServiceUnavailableComponent} from './service-unavailable/service-unavailable.component';
+import {ForbiddenComponent} from './forbidden/forbidden.component';
+import {UnauthorizedComponent} from './unauthorized/unauthorized.component';
+import {InternalServerErrorComponent} from './internal-server-error/internal-server-error.component'
+
 import {ExitGuard, RoleGuard} from '@shared/guards';
 
 const routes: Routes = [
   {
-    path: 'access-denied', component: AccessDeniedComponent
+    path: 'forbidden', component: ForbiddenComponent
   },
   {
     path: 'not-found', component: NotFoundComponent,
   },
   {
-    path: 'under-maintenance', component: UnderMaintenanceComponent
+    path: 'service-unavailable', component: ServiceUnavailableComponent
+  },
+  {
+    path: 'unauthorized', component: UnauthorizedComponent
+  },
+  {
+    path: 'internal-server-error', component: InternalServerErrorComponent
   }
 ];
 
