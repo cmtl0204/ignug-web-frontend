79bd9820bf6d09474e0b86be9b58e0f1699c7576
mgjurresta6 <mgj.urresta@yavirac.edu.ec>
2023-11-01 12:27:34 -0500

    enrollment-detail

--- src/app/models/core/enrollment-detail.model.ts
+++ src/app/models/core/enrollment-detail.model.ts
@@ -1,5 +1,35 @@
-import {CatalogueModel, LocationModel} from '@models/core';
+import {CatalogueModel, EnrollmentModel, GradeModel, SubjectModel} from '@models/core';
 
 export interface EnrollmentDetailModel {
     id?: string;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date;
+
+    grades: GradeModel[];
+    enrollmentDetailStates: EnrollmentDetailModel[];
+
+    //foreingkeys
+    academicState: CatalogueModel;
+    enrollment: EnrollmentModel;
+    parallel: CatalogueModel;
+    subject: SubjectModel;
+    type: CatalogueModel;
+    workday: CatalogueModel;
+
+    academicObservation: string;
+    number: number;
+    date: Date;
+    finalAttendance: number;
+    finalGrade: number;
+    observation: string;
+}
+
+export interface CreateEnrollmentDetailDto extends Omit<EnrollmentDetailModel, 'id'> {
+}
+
+export interface UpdateEnrollmentDetailDto extends Partial<Omit<EnrollmentDetailModel, 'id'>> {
+}
+
+export interface SelectEnrollmentDetailDto extends Partial<EnrollmentDetailModel> {
 }
